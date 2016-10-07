392f6ab71ad1bd4175b7dd0daf133eb5687f7a67
Emperor <5894672@qq.com>
2016-10-07 15:08:39 +0800

    构造SOAP请求消息
    构造SOAP请求消息

--- SoapHelper/ViewController.m
+++ SoapHelper/ViewController.m
@@ -4,7 +4,13 @@
 //
 //  Created by 陈煌 on 16/10/7.
 //  Copyright © 2016年 VCompany. All rights reserved.
-//
+
+
+//   客户端调用Web Service的基本过程
+//-- 构造SOAP请求消息（将本地数据对象转换为SOAP消息）
+//-- 发送SOAP消息到Web Service服务器的指定端口
+//-- 接收SOAP响应消息
+//-- 将SOAP响应消息转换为本地数据对象
 
 #import "ViewController.h"
 
